abca8caa90d29e08bbb733e91585e6435af4f871
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2026-02-06 16:21:26 -0300

    teste de aplicação

--- src/src/aplication/Main.java
+++ src/src/aplication/Main.java
@@ -1,17 +1,87 @@
 package aplication;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import UI.UIFactory;
+import UI.UIMobileFactory;
+import UI.UIWebFactory;
+import UI.componentesUI.Botao;
+import UI.componentesUI.Comportamento;
+import UI.componentesUI.Input;
+import config.AppConfig;
+import notification.EmailNotificationFactory;
+import notification.NotificationFactory;
+import notification.PushNotificationFactory;
+import notification.SMSNotificationFactory;
+import user.*;
+import user.constante.Genero;
+import user.constante.TipoDeUsuario;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        AppConfig config = AppConfig.getInstance();
+
+        ////////////////////////////////// UI
+
+        UIFactory uiFactory = switch (config.getTipoPlataforma().toUpperCase()){
+            case "WEB" -> new UIWebFactory();
+            case "MOBILE" -> new UIMobileFactory();
+            default -> throw new IllegalArgumentException("Erro: a plaforma é incompatível ou não existe!");
+        };
+
+        Botao botao = uiFactory.criarBotao();
+        botao.usarBotao();
+
+        Input input = uiFactory.criarInput();
+        input.usarInput();
+
+        Comportamento comportamento = uiFactory.criarComportamento();
+        comportamento.usarComportamento();
+
+        ////////////////////////////////// USUÁRIO
+
+        TipoDeUsuario userType = TipoDeUsuario.CLIENTE;
+
+        FactoryUsuario factoryUser = switch (userType){
+            case ADMIN -> new AdminFactoryUsuario();
+            case CLIENTE -> new ClienteFactoryUsuario();
+            default -> throw new IllegalArgumentException("Erro: Tipo de cliente não definido!");
+        };
+
+        Set<FactoryUsuario> setFactory = new HashSet<>();
+        setFactory.add(factoryUser);
+
+        UsuarioFactoryResolver resolver = new UsuarioFactoryResolver(setFactory);
+
+        Usuario user = resolver.criar( userType, new UsuarioCliente.Builder()
+                .nome("Pedro Paralamas")
+                .idade(40)
+                .sexo(Genero.MASCULINO)
+                .email("pedroparalama12@cliente.com")
+                .senha("1234")
+                .numeroDeTelefone("8799752345"));
+
+        System.out.println("Usuário "+ user.getNome()+" foi criado com sucesso!");
+
+        ////////////////////////////////// NOTIFICAÇÃO
+
+        NotificationFactory notificationFactory;
+
+        if (config.getTipoPlataforma().equalsIgnoreCase("WEB")){
+            notificationFactory = new EmailNotificationFactory();
+        }else if (config.getTipoPlataforma().equalsIgnoreCase("MOBILE")){
+            notificationFactory = new SMSNotificationFactory();
+        }else {
+            notificationFactory = new PushNotificationFactory();
         }
+
+        String mensagem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. \nLorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.\n It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.";
+
+        String notificacao = notificationFactory.processarNotificacao(user,mensagem);
+        System.out.println("===============================");
+        System.out.println("\n"+notificacao);
+        System.out.println("===============================");
     }
 }
