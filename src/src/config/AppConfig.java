5d3ca53fff6f0c0b803ffc93bddd3245a99c9a98
Matheus Eugênio Granja <eugeniomatheus977@gmail.com>
2026-02-06 16:19:41 -0300

    reajuste nos atributos de AppConfig

--- src/src/config/AppConfig.java
+++ src/src/config/AppConfig.java
@@ -1,4 +1,23 @@
 package config;
 
 public class AppConfig {
+
+    private String tipoPlataforma;
+
+    private AppConfig() {
+        this.tipoPlataforma = "WEB";
+    }
+
+    // CONFIG GLOBAL ÚNICA = SINGLETON
+    public static final class Holder{
+        public static final AppConfig INSTANCE = new AppConfig();
+    }
+
+    public static AppConfig getInstance(){
+        return Holder.INSTANCE;
+    }
+
+    public String getTipoPlataforma() {
+        return tipoPlataforma;
+    }
 }
